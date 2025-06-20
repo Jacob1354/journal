c1c830235aa6b8f61c891d73bb4cc4645088bc9a
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-06-19 20:13:48 -0400

    Refractor: Refractor AbstractMoodField to it's own file

--- first-proto/js/day.js
+++ first-proto/js/day.js
@@ -1,12 +1,14 @@
+import * as MoodField from "./mood_field";
+
 export class Day {
     constructor() {
         this.date = new Date();
         this.schedule = new Schedule();
         this.mood_fields = [];
-        this.mood_fields.push(new NumberField("Hours of sleep"));
-        this.mood_fields.push(new RatingField("Energy rating", 10));
-        this.mood_fields.push(new RatingField("Hapiness rating", 10));
-        this.mood_fields.push(new TextField("Comments", "Add any additional info here"));
+        this.mood_fields.push(new MoodField.NumberField("Hours of sleep"));
+        this.mood_fields.push(new MoodField.FractionField("Energy rating", 10));
+        this.mood_fields.push(new MoodField.FractionField("Hapiness rating", 10));
+        this.mood_fields.push(new MoodField.TextField("Comments", "Add any additional info here"));
     }
 }
 
@@ -17,25 +19,6 @@ class Schedule {
 }
 
 
-class AbstractMoodField {
-    #field_name;
-    
 
-    constructor(field_name) {
-        if(new.target === MoodField) {
-            throw new Error("Cannot initiate AbstactMoodField since it's an abstract class");
-        }
-        this.#field_name = field_name;
-    }
 
-    rename_field(new_field_name) {
-        this.#field_name = new_field_name;
-    }
-
-    get_field_name() {
-        return this.#field_name;
-    }
-
-    get_data() {}
-}
 

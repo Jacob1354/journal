ee6300f34773a182309aab0e80d095a44b6414ba
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-06-20 01:39:16 -0400

    Test(mood_field): Test AbstractMoodField instanciation

--- first-proto/js/mood_field.test.js
+++ first-proto/js/mood_field.test.js
@@ -2,6 +2,10 @@ import * as MoodField from "./mood_field";
 
 class TestField extends MoodField.AbstractMoodField {}
 
+test("Instanciating AbstractMoodField", () => {
+    expect(() => MoodField.AbstractMoodField("").toThrow(MoodField.AbstractClassInstanciated));
+});
+
 test("AbstractMoodFieldChild without set_data overriding", () => {
     let test_field = new TestField("Test");
     expect(() => test_field.set_data("").toThrow(MoodField.AbstractFunctionNotOverriden));

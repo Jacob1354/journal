7ee4bf6e5012905284139437ca82925d3115fc1d
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-06-28 13:46:52 -0400

    Fix(day): Add the namespace required to call Schedule()

--- first-proto/js/day.js
+++ first-proto/js/day.js
@@ -1,23 +1,16 @@
 import * as MoodField from "./mood_field";
+import * as Schedule from "./schedule"
 
 export class Day {
     constructor() {
         this.date = new Date();
-        this.schedule = new Schedule();
+        this.schedule = new Schedule.Schedule();
         this.mood_fields = [];
         this.mood_fields.push(new MoodField.NumberField("Hours of sleep"));
         this.mood_fields.push(new MoodField.FractionField("Energy rating"));
         this.mood_fields.push(new MoodField.FractionField("Hapiness rating"));
         this.mood_fields.push(new MoodField.TextField("Comments", "Add any additional info here"));
     }
-}
-
-class Schedule {
-    #activities;
-    constructor() {
-        this.#activities = new Map();
-    }
-    
 }
 
 

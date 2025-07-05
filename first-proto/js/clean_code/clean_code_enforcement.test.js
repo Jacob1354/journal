770e3ca575b2922ca5aa6f17dbaee615615716f5
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-07-05 18:37:16 -0400

    Refractor(cc_enforment): Change the imports in the test from * as to importing directly the functions and errors for clarity

--- first-proto/js/clean_code/clean_code_enforcement.test.js
+++ first-proto/js/clean_code/clean_code_enforcement.test.js
@@ -1,42 +1,49 @@
-import * as CCEnforcement from "./clean_code_enforcement";
+import {
+    validate_type,
+    validate_integer,
+    validate_array,
+    validate_array_type,
+    InvalidDataType,
+    OutOfBoundInteger,
+    ArrayContaintsInvalidDataType
+} from "./clean_code_enforcement.js";
 
 test("validate_type primitives test", () => {
-    expect(CCEnforcement.validate_type("test", "string")).toBe(true);
-    expect(() => CCEnforcement.validate_type(new String(1), "string")).toThrow(CCEnforcement.InvalidDataType);
-    expect(() => CCEnforcement.validate_type(1, "string")).toThrow(CCEnforcement.InvalidDataType);
+    expect(validate_type("test", "string")).toBe(true);
+    expect(() => validate_type(new String(1), "string")).toThrow(InvalidDataType);
+    expect(() => validate_type(1, "string")).toThrow(InvalidDataType);
 });
 
 test("validate_type classes test", () => {
-    expect(CCEnforcement.validate_type(new Map(), Map)).toBe(true);
-    expect(() => CCEnforcement.validate_type(new String(1), Number)).toThrow(CCEnforcement.InvalidDataType);
-    expect(() => CCEnforcement.validate_type(1, Number)).toThrow(CCEnforcement.InvalidDataType);
+    expect(validate_type(new Map(), Map)).toBe(true);
+    expect(() => validate_type(new String(1), Number)).toThrow(InvalidDataType);
+    expect(() => validate_type(1, Number)).toThrow(InvalidDataType);
 });
 
 test("validate_integer test", () => {
-    expect(CCEnforcement.validate_integer(10)).toBe(true);
-    expect(() => CCEnforcement.validate_integer(new String(1))).toThrow(CCEnforcement.InvalidDataType);
-    expect(() => CCEnforcement.validate_integer(1.5)).toThrow(CCEnforcement.InvalidDataType);
+    expect(validate_integer(10)).toBe(true);
+    expect(() => validate_integer(new String(1))).toThrow(InvalidDataType);
+    expect(() => validate_integer(1.5)).toThrow(InvalidDataType);
 })
 test("validate_integer out of bounds", () => {
-    expect(CCEnforcement.validate_integer(10, 0, 100)).toBe(true);
-    expect(() => CCEnforcement.validate_integer(-10, 0, 100)).toThrow(CCEnforcement.OutOfBoundInteger);
-    expect(() => CCEnforcement.validate_integer(1000, -10, 0)).toThrow(CCEnforcement.OutOfBoundInteger);
+    expect(validate_integer(10, 0, 100)).toBe(true);
+    expect(() => validate_integer(-10, 0, 100)).toThrow(OutOfBoundInteger);
+    expect(() => validate_integer(1000, -10, 0)).toThrow(OutOfBoundInteger);
 })
 
 test("validate_array", () => {
     const arr = [];
     const not_arr = 1;
     
-    expect(() => CCEnforcement.validate_array(arr)).not.toThrow(CCEnforcement.InvalidDataType);
-    expect(() => CCEnforcement.validate_array(not_arr)).toThrow(CCEnforcement.InvalidDataType);
+    expect(() => validate_array(arr)).not.toThrow(InvalidDataType);
+    expect(() => validate_array(not_arr)).toThrow(InvalidDataType);
 });
 
 test("validate_array_type", () => {
     const valid_str_arr = ["a", "b", "c", "d", "e"]; 
     const invalid_str_arr = ["a", "b", 1, new String(), "e"];
 
-    expect(() => CCEnforcement.validate_array_type(valid_str_arr, "string")).not.toThrow(CCEnforcement.ArrayContaintsInvalidDataType);
-
-    expect(() => CCEnforcement.validate_array_type(invalid_str_arr, "string")).toThrow(CCEnforcement.ArrayContaintsInvalidDataType);
+    expect(() => validate_array_type(valid_str_arr, "string")).not.toThrow(ArrayContaintsInvalidDataType);
+    expect(() => validate_array_type(invalid_str_arr, "string")).toThrow(ArrayContaintsInvalidDataType);
 });
 

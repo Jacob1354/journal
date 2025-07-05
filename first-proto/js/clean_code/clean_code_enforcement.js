a3ab9768e074cfdf716ff0a2b5e8a4e4c2548f8e
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-07-05 18:27:08 -0400

    Feat(cc_enforcement): Add validate_array_type

--- first-proto/js/clean_code/clean_code_enforcement.js
+++ first-proto/js/clean_code/clean_code_enforcement.js
@@ -37,12 +37,41 @@ export function validate_integer(value, min = Number.MIN_SAFE_INTEGER, max = Num
     return true;
 }
 
+export function validate_array_type(array, type) {
+    validate_array(array);
+    array.forEach(element => {
+        try {
+            validate_type(element, type);
+        } catch (err) {
+            if(err instanceof InvalidDataType || err instanceof TypeError)
+                throw new ArrayContaintsInvalidDataType(
+                    `This array contains a value that isn't a ${typeof type === "function" ? type.name : type}`
+                );
+            else
+                throw err;
+        }
+    });
+}
+
+export function validate_array(array) {
+    if(!Array.isArray(array))
+        throw new InvalidDataType("This variable isn't an array");
+}
+
 export class InvalidDataType extends Error {
     constructor(msg) {
         super(msg);
         this.name = "InvalidDataType"
     }
 }
+
+export class ArrayContaintsInvalidDataType extends Error {
+    constructor(msg) {
+        super(msg);
+        this.name = "ArrayContaintsInvalidDataType"
+    }
+}
+
 
 export class OutOfBoundInteger extends Error {
     constructor(msg) {

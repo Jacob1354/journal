25377bb31bbdaf70dd6ccfa4edf06e92f2a0ed46
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-07-09 16:21:49 -0400

    Feat(dom/mood_fields): Add the load_fields functions

---
+++ first-proto/js/dom/mood_field.js
@@ -0,0 +1,92 @@
+import { validate_type } from "../clean_code/clean_code_enforcement";
+import { AbstractMoodField, FractionField, NumberField, SliderField, TextField } from "../data/mood_field";
+import { validate } from "C:/Users/Jacob/AppData/Local/Microsoft/TypeScript/5.8/node_modules/@babel/types/lib/index";
+
+
+export function load_field(field) {
+    validate_type(field, AbstractMoodField);
+    if(field instanceof NumberField) {
+        return _load_number_field(field);
+    } else if(field instanceof TextField) {
+        return _load_text_field(field);
+    } else if(field instanceof FractionField) {
+        return _load_fraction_field(field);
+    } else if(field instanceof SliderField) {
+        return _load_slider_field(field);
+    }
+}
+
+export function _load_number_field(field) {
+    validate_type(field, NumberField);
+
+    const mood_field_wrapper = create_mood_field_wrapper_with_title(field);
+    const nb_field = document.createElement("div");
+    nb_field.classList.add("mood_field_nb");
+    const input = document.createElement("input");
+    input.type = "number";
+    input.value = field.get_data();
+    nb_field.appendChild(input);
+    mood_field_wrapper.appendChild(nb_field);
+
+    return mood_field_wrapper;
+}
+
+export function _load_text_field(field) {
+    validate_type(field, TextField);
+
+    const mood_field_wrapper = create_mood_field_wrapper_with_title(field);
+    const text_field = document.createElement("div");
+    text_field.classList.add("mood_field_text");
+    const input = document.createElement("textarea");
+    input.name = "mood_field_text";
+    input.value = field.get_data();
+    text_field.appendChild(input);
+    mood_field_wrapper.appendChild(text_field);
+
+    return mood_field_wrapper;
+}
+
+export function _load_fraction_field(field) {
+    validate_type(field, FractionField);
+
+    const mood_field_wrapper = create_mood_field_wrapper_with_title(field);
+    const fraction_field = document.createElement("div");
+    fraction_field.classList.add("mood_field_fraction");
+    const input = document.createElement("input");
+    input.type = "number";
+    input.value = field.get_data();
+    const denominator = document.createElement("p");
+    denominator.innerText = "/" + String(field.get_denominator());
+    fraction_field.appendChild(input);
+    fraction_field.appendChild(denominator);
+    mood_field_wrapper.appendChild(fraction_field);
+
+    return mood_field_wrapper;
+}
+
+export function _load_slider_field(field) {
+    validate_type(field, SliderField);
+
+    const mood_field_wrapper = create_mood_field_wrapper_with_title(field);
+    const nb_field = document.createElement("div");
+    nb_field.classList.add("mood_field_slider");
+    const input = document.createElement("input");
+    input.type = "range";
+    input.value = field.get_data();
+    nb_field.appendChild(input);
+    mood_field_wrapper.appendChild(nb_field);
+
+    return mood_field_wrapper;
+}
+
+function create_mood_field_wrapper_with_title(field) {
+    validate_type(field, AbstractMoodField);
+
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("mood_field");
+    const field_name = document.createElement("h2");
+    field_name.innerText = field.get_field_name();
+    wrapper.appendChild(field_name);
+    
+    return wrapper;
+}
---
+++ first-proto/js/dom/mood_field.test.js
@@ -0,0 +1,69 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { FractionField, NumberField, SliderField, TextField } from "../data/mood_field";
+import { _load_fraction_field, _load_number_field, _load_slider_field, _load_text_field, load_field } from "./mood_field";
+import { validate } from "C:/Users/Jacob/AppData/Local/Microsoft/TypeScript/5.8/node_modules/@babel/types/lib/index";
+
+let nb_field, fraction_field, slider_field, text_field;
+
+beforeAll(() => {
+    nb_field = new NumberField("nb_field");
+    fraction_field = new FractionField("fraction_field");
+    slider_field = new SliderField("slider_field");
+    text_field = new TextField("text_field");
+})
+
+
+test("load_field", () => {
+}); 
+
+//Field type must be one of the following : mood_field_[nb/text/fraction/slider]
+function test_field_basics(field_el, field_data, field_type) {
+    validate(field_type, "string");
+
+    expect(field_el.classList.contains("mood_field")).toBe(true);
+    expect(field_el.children[0].nodeName).toBe("H2");
+    expect(field_el.children[0].innerText).toBe(field_data.get_field_name());
+    expect(field_el.children[1].nodeName).toBe("DIV");
+    expect(field_el.children[1].classList.contains("mood_field_" + field_type)).toBe(true);
+}
+
+test("_load_number_field", () => {
+    const nb_field_el = _load_number_field(nb_field);
+
+    test_field_basics(nb_field_el, nb_field, "nb");
+    expect(nb_field_el.children[1].children[0].nodeName).toBe("INPUT");
+    expect(nb_field_el.children[1].children[0].type).toBe("number");
+    expect(nb_field_el.children[1].children[0].value).toBe(String(nb_field.get_data()));
+}); 
+
+test("_load_text_field", () => {
+    const text_field_el = _load_text_field(text_field);
+    test_field_basics(text_field_el, text_field, "text");
+
+    expect(text_field_el.children[1].children[0].nodeName).toBe("TEXTAREA");
+    expect(text_field_el.children[1].children[0].name).toBe("mood_field_text");
+    expect(text_field_el.children[1].children[0].value).toBe(text_field.get_data());
+}); 
+
+test("_load_fraction_field", () => {
+    const fraction_field_el = _load_fraction_field(fraction_field);
+    test_field_basics(fraction_field_el, fraction_field, "fraction");
+
+    expect(fraction_field_el.children[1].children[0].nodeName).toBe("INPUT");
+    expect(fraction_field_el.children[1].children[0].type).toBe("number");
+    expect(fraction_field_el.children[1].children[0].value).toBe(String(nb_field.get_data()));
+    expect(fraction_field_el.children[1].children[1].nodeName).toBe("P");
+    expect(fraction_field_el.children[1].children[1].innerText).toBe("/" + String(fraction_field.get_denominator()));
+}); 
+
+test("load_slider_field", () => {
+    const slider_field_el = _load_slider_field(slider_field);
+
+    test_field_basics(slider_field_el, slider_field, "slider");
+    expect(slider_field_el.children[1].children[0].nodeName).toBe("INPUT");
+    expect(slider_field_el.children[1].children[0].type).toBe("range");
+    expect(slider_field_el.children[1].children[0].value).toBe(String(slider_field.get_data()));
+}); 

5f58cbf52c77ed135a581ea1cf201e976a003299
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-07-09 22:17:22 -0400

    Feat(dom_utils): Add place_elements_until_field1_max_then_field2

--- first-proto/js/dom/dom_utils.js
+++ first-proto/js/dom/dom_utils.js
@@ -1,8 +1,40 @@
-import { validate_type } from "../clean_code/clean_code_enforcement.js";
+import { validate_array_type, validate_type } from "../clean_code/clean_code_enforcement.js";
 
 export function clear_element_children(element) {
     validate_type(element, HTMLElement);
     while(element.firstChild) {
         element.removeChild(element.firstChild);
     }
+}
+
+
+export function place_elements_until_field1_max_then_field2(field_1_max_height, field_1, field_2, elements) {
+    validate_type(field_1_max_height, "number");
+    validate_type(field_1, HTMLElement);
+    validate_type(field_2, HTMLElement);
+    validate_array_type(elements, HTMLElement);
+
+    let field_1_height = field_1.getBoundingClientRect().height;
+    let place_in_field_1 = true;
+    
+    elements.forEach(el => {
+        if(place_in_field_1){
+            field_1.appendChild(el);
+            field_1_height = field_1.getBoundingClientRect().height;
+            if(field_1_height > field_1_max_height) { 
+                move_child_node(field_1.lastChild, field_2);
+                place_in_field_1 = false;
+            }
+        } else {
+            field_2.appendChild(el);
+        }
+    });
+}
+
+export function move_child_node(el, new_parent) {
+    validate_type(el, HTMLElement);
+    validate_type(new_parent, HTMLElement);
+
+    el.parentElement.removeChild(el);
+    new_parent.appendChild(el);
 }
--- first-proto/js/dom/dom_utils.test.js
+++ first-proto/js/dom/dom_utils.test.js
@@ -17,3 +17,66 @@ test("clear_element", () => {
     expect(origin.childElementCount).toBe(0);
 
 })
+
+
+function mock_getBoundingClientRect(el = new HTMLElement) {
+    el.getBoundingClientRect = () => {
+        let height = 0;
+        for(const child of el.children) {
+            height += child.getBoundingClientRect().height;
+        };
+        return {
+        width: 10,
+        height: height,
+        top: 0,
+        left: 0,
+        bottom: height,
+        right: 10,
+        x: 0,
+        y: 0,
+        toJSON: () => {},
+        };
+    }
+}
+
+test("place_elements_until_field1_max_then_field2", () => {
+    const elements = Array.from({ length: 10 }, () => {
+        const el = document.createElement("div");
+        el.getBoundingClientRect = () => ({
+            width: 10,
+            height: 100, //Only important part is height
+            top: 0,
+            left: 0,
+            bottom: 100,
+            right: 10,
+            x: 0,
+            y: 0,
+            toJSON: () => {},
+        });
+        document.body.appendChild(el);
+        return el;
+    });
+
+    const field_1 = document.createElement("div");
+    mock_getBoundingClientRect(field_1);
+    const field_2 = document.createElement("div");
+    mock_getBoundingClientRect(field_2);
+    const max_height = 400;
+
+    DOMUtils.place_elements_until_field1_max_then_field2(max_height, field_1, field_2, elements);
+
+    expect(field_1.children.length).toBe(4);
+    expect(field_2.children.length).toBe(6);
+    
+});
+
+test("move_child_node", () => {
+    const field_1 = document.createElement("div");
+    const field_2 = document.createElement("div");
+    const child = document.createElement("div");
+
+    field_1.appendChild(child);
+    DOMUtils.move_child_node(child, field_2);
+    expect(field_1.firstChild).toBe(null);
+    expect(field_2.firstChild).toBe(child);
+});

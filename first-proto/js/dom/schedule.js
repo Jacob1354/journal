80a5e39ff803f7f55d40b94d4b1968a8a2f27c32
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-07-09 22:54:04 -0400

    Fix(dom/schedule): Add a '#' to the query selecor of render_scheduled_activities so it gets the id

--- first-proto/js/dom/schedule.js
+++ first-proto/js/dom/schedule.js
@@ -11,7 +11,7 @@ export const removeBtnHTML = `<svg width="20" height="20" viewBox="0 0 24 24" fi
 
 export function render_scheduled_activities(activities) {
     validate_array_type(activities, Activity);
-    document.querySelector("scheduled_activities").replaceWith(create_scheduled_activities(activities));
+    document.querySelector("#scheduled_activities").replaceWith(create_scheduled_activities(activities));
 }
 
 export function create_scheduled_activities(activities) {

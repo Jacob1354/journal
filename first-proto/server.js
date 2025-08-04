6968a3f6e84698a3958e215b5b502daa593dbbe3
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-04 16:55:59 -0400

    Feat(db): Add pg and basic db related basic code

--- first-proto/server.js
+++ first-proto/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { access, exists, existsSync, readFile } from 'node:fs'
+import { access, existsSync, readFile } from 'node:fs'
 import { constants } from 'node:fs/promises';
 import { extname } from 'node:path';
+import { SERVER_HOST, SERVER_PORT } from './server_const.js';
+import { get_client_from_pool, load_db_pool } from './db_interation.js';
+
 
 const app = express();
-const host = "127.0.0.1";
-const port = 3000;
+
 
 const MIME_type = {
     ".html" : "text/html",
@@ -17,20 +19,20 @@ app.get('/', async (req, res, next) => {
     await readFile("./day.html", "utf8", (err, data) => {
         if(err) throw err;
         res.send(data);
-    });;
+    });
+
 });
 
 app.use(async (req, res, next) => {
     const referer = req.headers["referer"] || '';
     const path = "." + req.url;
     const ext = extname(path);
-    if(!referer.includes(host) || access(path, constants.F_OK, (err) => err)) {
+    if(!referer.includes(SERVER_HOST) || access(path, constants.F_OK, (err) => err)) {
         res.status(404).send("404 - Ressource not found");
     }
     else {
         await readFile(path, "utf8", (err, data) => {
             if(err) throw err;
-            console.log(path);
             res.set('Content-type', MIME_type[ext]);
             res.send(data);
         })
@@ -38,6 +40,6 @@ app.use(async (req, res, next) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Server listening on http://${host}:${port}/`)
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening on http://${SERVER_HOST}:${SERVER_PORT}/`)
 })
---
+++ first-proto/server_const.js
@@ -0,0 +1,5 @@
+export const SERVER_HOST = "127.0.0.1";
+export const SERVER_PORT = 3000;
+***REMOVED***
+***REMOVED***
+***REMOVED***

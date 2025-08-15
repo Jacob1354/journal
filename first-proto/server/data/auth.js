afbeac51794df70729d5e8558077fb644c389bda
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-14 21:26:21 -0400

    Feat(server/data/user): Add the user data structure

---
+++ first-proto/server/data/auth.js
@@ -0,0 +1,13 @@
+import { validate_type } from "first-proto/shared/clean_code/clean_code_enforcement";
+
+export class User {
+    constructor({username, password, name = null}) {
+        validate_type(username, "string");
+        validate_type(password, "string");
+        if(name != null)
+            validate_type(name, "string");
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
+}

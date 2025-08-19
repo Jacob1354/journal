9028fb678f5898fca9f07599f562ef56e856790b
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-19 15:25:10 -0400

    Refractor: Change methods positions

--- first-proto/server/dao/auth_dao.js
+++ first-proto/server/dao/auth_dao.js
@@ -7,16 +7,16 @@ export class AuthDAO {
         validate_type(db, Database);
         this.#db = db;
     }
+    
+    get_user(username) {
+        throw new Error("Not implemented");
+    }
 
     get_user_from_session(session) {
         throw new Error("Not implemented");
     }
 
     add_user(user) {
-        throw new Error("Not implemented");
-    }
-
-    get_user(username) {
         throw new Error("Not implemented");
     }
 

e1a31f80f69a2f3322211dff28e6223c606a30d8
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-20 14:56:23 -0400

    Feat(server/auth_data): Add a hash property to the User class

--- first-proto/server/data/auth_data.js
+++ first-proto/server/data/auth_data.js
@@ -1,13 +1,17 @@
 import { validate_type } from "../../shared/clean_code/clean_code_enforcement";
 
 export class User {
-    constructor({username, password, name = null}) {
+    constructor({username, password = null, hash = null, name = null}) {
         validate_type(username, "string");
-        validate_type(password, "string");
+        if(password != null)
+            validate_type(password, "string");
+        if(hash != null)
+            validate_type(hash, "string");
         if(name != null)
             validate_type(name, "string");
         this.username = username;
         this.password = password;
         this.name = name;
+        this.hash = hash;
     }
 }

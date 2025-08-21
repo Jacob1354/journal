8e2b479b8bcb3a29ae6443642de4c6210438c6c2
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-21 16:43:17 -0400

    Refractor(public/signin.js): Refractor code in more functions

--- first-proto/public/js/sign_in_up/signin.js
+++ first-proto/public/js/sign_in_up/signin.js
@@ -1,3 +1,5 @@
+import { User } from "first-proto/shared/data/user";
+
 function validate_form(form) {
     const inputs = form.getElementsByTagName("input");
     for(let i = 0;  i < inputs.length; i++)
@@ -7,46 +9,59 @@ function validate_form(form) {
     return true;
 }
 
+function get_user_from_form() {
+    const username = document.getElementById("username_input").value;
+    const password = document.getElementById("password_input").value;
+    const user = new User({
+        username: username,
+        password: password
+    });
+    return user;
+}
+
+function handle_signin_response(res) {
+    if(res.status < 200 || res.status > 299) {
+        res.text()
+            .then((text) => {
+                document.getElementById("signin_err").innerText = text;
+                document.getElementById("signin_success").innerText = "";
+            })
+    }
+    else {
+        res.json()
+        .then((obj) => {
+            document.getElementById("signin_success").innerText = 
+                "Welcome " + obj.name + "!";
+                document.getElementById("signin_err").innerText = "";
+            })
+    }
+}
+
+function handle_signin_error(err) {
+    console.log(err);
+    document.getElementById("signin_err").innerText = 
+    "An error occured and we couldn't sign you in sorry :/";
+    document.getElementById("signin_success").innerText = "";
+}
+
+
 document.getElementById("sign_in_btn").addEventListener("click", (e) => {
-        const form = e.currentTarget.closest("form");
-        if(validate_form(form)) {
-            const username = document.getElementById("username_input").value;
-            const password = document.getElementById("password_input").value;
-            const user = { "username": username, "pwd": password};
-            fetch("/signin", {
-                method: "POST",
-                body: JSON.stringify(user),
-                headers: {
-                    "Content-type": "application/json"
-                }
-                })
-                .then((res) => {
-                    if(res.status < 200 || res.status > 299) {
-                        res.text()
-                            .then((text) => {
-                                document.getElementById("signin_err").innerText = text;
-                                document.getElementById("signin_success").innerText = "";
-                            })
-                        }
-                        else {
-                            res.json()
-                            .then((obj) => {
-                                document.getElementById("signin_success").innerText = 
-                                    "Welcome " + obj.name + "!";
-                                    document.getElementById("signin_err").innerText = "";
-                                })
-                            }
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                            document.getElementById("signin_err").innerText = 
-                            "An error occured and we couldn't sign you in sorry :/";
-                            document.getElementById("signin_success").innerText = "";
-                        });
-        } 
-        else {
-            document.getElementById("signin_err").innerText = "Error ! Cannot send a form with empty fields";
-            document.getElementById("signin_success").innerText = "";
-        }
-    })
+    const form = e.currentTarget.closest("form");
+    if(validate_form(form)) {
+        const user = get_user_from_form();
+        fetch("/signin", {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+        .then(handle_signin_response)
+        .catch(handle_signin_error);
+    } 
+    else {
+        document.getElementById("signin_err").innerText = "Error ! Cannot send a form with empty fields";
+        document.getElementById("signin_success").innerText = "";
+    }
+})
             

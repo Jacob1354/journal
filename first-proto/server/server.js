2f565bc16fb00f3e4aae3479dcfdcd768cf3dadc
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-21 15:48:12 -0400

    Feat(server): Add the basic gets for signin and signup

--- first-proto/server/server.js
+++ first-proto/server/server.js
@@ -1,34 +1,45 @@
 import express from 'express';
-import { SERVER_HOST, SERVER_PORT } from './server_const.js';
+import { HTML_DAY_PATH, HTML_SIGNIN_PATH, HTML_SIGNUP_PATH, SERVER_HOST, SERVER_PORT } from './server_const.js';
 import { JournalServer } from './journal_server.js';
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
 const journal_srv = new JournalServer();
 
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
 app.get('/', async (req, res, next) => {
-    res.redirect("/html/signin.html");
+    res.redirect("/signin");
 });
 
-app.post('/signin', (req, res) => {
-
+app.get('/signin', (req, res) => {
+    console.log("here");
+    if(req.cookies["session"]) {
+        const user = journal_srv.auth_service.authenticate_session(req.cookies["session"]);
+        res.redirect(HTML_DAY_PATH);
+    } else {
+        res.redirect(HTML_SIGNIN_PATH);
+    }
 });
 
-app.post('/signup', (req, res) => {
-
+app.get('/signup', (req, res) => {
+    res.redirect(HTML_SIGNUP_PATH);
 });
 
 app.use(express.static("public"));
 
 
 const server = app.listen(SERVER_PORT, () => {
-  console.log(`Server listening on http://${SERVER_HOST}:${SERVER_PORT}/`);
+    console.log(`Server listening on http://${SERVER_HOST}:${SERVER_PORT}/`);
 })
 
 server.on("close", (err) => {
-  console.log("closing server");
-  if(err) console.log(err);
-  journal_srv.close();
+    console.log("closing server");
+    if(err) console.log(err);
+    journal_srv.close();
 });
 
 process.on("SIGINT", () => server.close());
--- first-proto/server/server_const.js
+++ first-proto/server/server_const.js
@@ -4,3 +4,8 @@ export const DB_NAME = "journaldb.db";
 
 export const SQL_AUTH_SETUP_PATH = "./server/db_scripts/auth_setup.sql";
 export const SQL_JOURNAL_SETUP_PATH = "./server/db_scripts/journal_setup.sql";
+
+export const HTML_PAGES_PATH = "/html/";
+export const HTML_DAY_PATH = HTML_PAGES_PATH + "day.html";
+export const HTML_SIGNIN_PATH = HTML_PAGES_PATH + "signin.html";
+export const HTML_SIGNUP_PATH = HTML_PAGES_PATH + "signup.html";

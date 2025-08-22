f83bead471317fa25736a09bbf7767c71a249421
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-22 16:53:39 -0400

    Feat(auth_service): signin_user now returns a Cookie instead of only the session value

--- first-proto/server/services/auth_sevice.js
+++ first-proto/server/services/auth_sevice.js
@@ -4,10 +4,12 @@ import { randomBytes } from 'crypto';
 import { InvalidUser, User } from "../../shared/data/user.js";
 import Database from "better-sqlite3";
 import * as argon2 from 'argon2';
+import { Cookie } from "../../shared/data/cookie.js";
 
 
 export class AuthService {
     #auth_dao;
+    #session_max_age = 10; //TODO change this to 3600(1h). It currently is shorter for test purposes
     constructor(db) {
         validate_type(db, Database);
         this.#auth_dao = new AuthDAO(db);
@@ -34,9 +36,16 @@ export class AuthService {
         if(!await argon2.verify(db_user.hash, user.password))
             throw new InvalidPassword("Invalid password");
 
-        const session = randomBytes(32);
+        const session = randomBytes(32).toString("hex");
         this.#auth_dao.add_session(user, session);
-        return session;
+        const session_cookie = new Cookie({
+            name: "session",
+            value: session,
+            secure: false, //Not using https yet
+            path: "/",
+            max_age: this.#session_max_age
+        });
+        return session_cookie;
     }
 
     authenticate_session(session) {

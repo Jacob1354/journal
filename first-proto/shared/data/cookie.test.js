788a7de527d0b2f40be0227e7cee49fc237005a8
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-22 16:41:45 -0400

    Feat(data/cookie): Add a Cookie class to contain cookie info

---
+++ first-proto/shared/data/cookie.test.js
@@ -0,0 +1,19 @@
+import { Cookie } from "./cookie";
+
+test("Cookie.toString", ()=> {
+    let cookie = new Cookie({
+        name: "name",
+        value: "value"
+    })
+    expect(String(cookie)).toBe("name=value;HttpOnly;Secure;");
+
+    cookie = new Cookie({
+        name: "name",
+        value: "value",
+        path:"/",
+        max_age: 10,
+        secure: false,
+        http_only: false
+    })
+    expect(String(cookie)).toBe("name=value;Path=/;Max-Age=10;");
+})

7081fdbdf5edac44585436e9634799987641625c
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-23 18:36:53 -0400

    Feat(public/day.js): Implement get_date_from_url

--- first-proto/public/js/day.js
+++ first-proto/public/js/day.js
@@ -3,15 +3,22 @@ import { Day } from "../../shared/data/day.js";
 import {  } from "./dom/schedule.js";
 import { DomDay } from "./dom/dom_day.js"
 
-function get_day_from_url() {
-
+function get_date_from_url() {
+    const date_regex = /\d+-\d+-\d+/;
+    const url = window.location.href;
+    const date_array = url.match(date_regex)[0].match(/\d+/g); //Format: [day, month, year]
+    const date = new Date();
+    date.setFullYear(Number(date_array[2]));
+    date.setMonth(Number(date_array[1])-1); //-1 because jan is 0 for a weird reason
+    date.setDate(Number(date_array[0]));
+    return date;
 }
 
 function fetch_day(day) {
-    
+    return new Day();
 }
 
-let dom_day = new DomDay(new Day());
+let dom_day = new DomDay(fetch_day(get_date_from_url()));
 
 dom_day.render_day();
 

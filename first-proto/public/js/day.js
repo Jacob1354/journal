49acf320163e7e815c2f383cbf4e437c03824e8f
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-24 09:03:51 -0400

    Refractor/Fix(public/day_api): Move the functions get_date_from_url and fetch_day to a seperate file to make it testable

--- first-proto/public/js/day.js
+++ first-proto/public/js/day.js
@@ -1,24 +1,7 @@
-import { validate_type } from "../../shared/clean_code/clean_code_enforcement.js";
-import { Day } from "../../shared/data/day.js";
-import {  } from "./dom/schedule.js";
 import { DomDay } from "./dom/dom_day.js"
+import { fetch_day, get_date_from_url } from "./api/day_api.js";
 
-function get_date_from_url() {
-    const date_regex = /\d+-\d+-\d+/;
-    const url = window.location.href;
-    const date_array = url.match(date_regex)[0].match(/\d+/g); //Format: [day, month, year]
-    const date = new Date();
-    date.setFullYear(Number(date_array[2]));
-    date.setMonth(Number(date_array[1])-1); //-1 because jan is 0 for a weird reason
-    date.setDate(Number(date_array[0]));
-    return date;
-}
-
-function fetch_day(day) {
-    return new Day();
-}
-
-let dom_day = new DomDay(fetch_day(get_date_from_url()));
+let dom_day = new DomDay(fetch_day(get_date_from_url(window.location.href)));
 
 dom_day.render_day();
 

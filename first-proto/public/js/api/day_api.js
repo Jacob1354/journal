d7da6bc2d9c21faba739bdb2d8091cdba930b6b4
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-25 11:13:21 -0400

    Feat(public/day_api): Implement fetch_day

--- first-proto/public/js/api/day_api.js
+++ first-proto/public/js/api/day_api.js
@@ -1,8 +1,21 @@
 import { Day } from "../../../shared/data/day.js";
+import { error_pop_up } from "../dom/dom_utils.js";
 
-
-export async function fetch_day(day) {
-    return new Day();
+//TODO handle case where the promise is never settled
+export async function fetch_day(date = new Date()) {
+    const day_path = "/day/" + date.getDate() +"-"+ date.getMonth() +"-"+ date.getFullYear();
+    let response;
+    try {
+        response = await fetch(day_path);
+    } catch(err) {
+        error_pop_up("An error occured, sorry :/");
+    }
+    if(response.status == 200)
+        return Day.from(await response.json());
+    else if(response.status == 401)
+        throw new InvalidAuth("You must be logged to access this data");
+    else if(response.status == 500)
+        throw new ServerErr("Couldn't retrieve data due to a serer error");
 }
 
 

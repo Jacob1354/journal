6645217d88d2540133978bf5c0b09ae4502bc3ca
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-27 11:49:55 -0400

    Test(server/journal_server): Add the test for JournalServer.authenticate_session

---
+++ server/journal_server.test.js
@@ -0,0 +1,47 @@
+import { jest } from "@jest/globals";
+import { JournalServer } from "./journal_server";
+import { User } from "../shared/data/user";
+
+const received = {status: null};
+const valid_session = "valid_session";
+const invalid_session = "invalid_session";
+const user = new User({
+    username: "jacob"
+});
+
+
+const mockStatus = jest.fn(function (status) {received.status = status; return this});
+const mockSend = jest.fn(() => {});
+const MockResponse = jest.fn(() => ({
+    status: mockStatus,
+    send: mockSend
+}));
+
+const mockValidateType = jest.fn(() => true);
+jest.unstable_mockModule("../shared/clean_code/clean_code_enforcement", () => ({
+    validate_type: mockValidateType
+}));
+const {validate_type} = await import("../shared/clean_code/clean_code_enforcement");
+
+
+const journal_srv = new JournalServer();
+
+
+test("JournalServer.authenticate_session: sucess", () => {
+    const res = MockResponse();
+    expect(journal_srv.authenticate_session(valid_sesion, res)).toEqual(valid_user);
+});
+
+test("JournalServer.authenticate_session: InvalidSession", () => {
+    const res = MockResponse();
+    journal_srv.authenticate_session(undefined, res);
+    expect(received).toEqual({status: 401});
+    journal_srv.authenticate_session(invalid_session, res);
+    expect(received).toEqual({status: 401});
+});
+
+test("JournalServer.authenticate_session: InternalServerError", () => {
+    const res = MockResponse();s
+    journal_srv.authenticate_session(valid_session, res);
+    expect(received).toEqual({status: 500});
+});

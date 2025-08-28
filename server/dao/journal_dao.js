c02de52623af7ec3a0ab4ed5202531ba313f5368
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-28 17:44:56 -0400

    Feat(server/journal_dao): Define the public functions required by JournalService.get_day_json_obj

--- server/dao/journal_dao.js
+++ server/dao/journal_dao.js
@@ -1,10 +1,19 @@
 import Database from "better-sqlite3";
 import { validate_type } from "../../shared/clean_code/clean_code_enforcement.js";
+import { Day } from "../../shared/data/day.js";
 
 export class JournalDAO {
     #db;
     constructor(db) {
         validate_type(db, Database);
         this.#db = db;
+    }
+
+    create_day(user, date) {
+
+    }
+
+    get_day(user, date) {
+        return new Day();
     }
 }

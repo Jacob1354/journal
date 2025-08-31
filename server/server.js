a4981833e23058515685d5e2c6c4a4cf5f9cc9ea
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-08-30 22:36:28 -0400

    Revert "Chore: Install and add whyIsNodeRunning to server.js since it often happens for the server to keep running after sigint"
    This reverts commit 13baeb2fd062d3a58847db604b9fa4cf983df705.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import whyIsNodeRunning from 'why-is-node-running';
 import express from 'express';
 import { ERR_MSG_SERVER_ERR, ERR_MSG_SIGN_IN_INVALID_USER, ERR_MSG_UNAVAILABLE_USERNAME, HTML_DAY_PATH, HTML_SIGNIN_PATH, HTML_SIGNUP_PATH, SERVER_HOST, SERVER_PORT } from './server_const.js';
 import { JournalServer } from './journal_server.js';
@@ -103,7 +102,6 @@ server.on("close", (err) => {
     } catch(err) {
         console.log(err);
     }
-    setImmediate(() => whyIsNodeRunning());
 });
 
 process.on("SIGINT", () => server.close());

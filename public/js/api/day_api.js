80c9db3ba6b2a04fc8b512f57fbc1289cce24d81
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-09-03 12:03:54 -0400

    Fix(public/day_api): Correct the body sent with fetch in post_day. It used to send the object directly. Now it stringifies it first

--- public/js/api/day_api.js
+++ public/js/api/day_api.js
@@ -20,7 +20,7 @@ export async function post_day(day) {
     validate_type(day, Day);
     const url = "/day/" + day.date.getDate() + "-" + day.date.getMonth() + "-" + day.date.getFullYear();
     const info = {
-        body: day.prepare_json_obj(),
+        body: JSON.stringify(day.prepare_json_obj()),   
         method: "POST",
         headers: {
                 "Content-type": "application/json"

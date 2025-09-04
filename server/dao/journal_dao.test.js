5f56fe6f7566d6e4ce101ab919845b6d5b7ee82d
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-09-04 19:41:25 -0400

    Refractor(server/journal_dao.test): Refractor tests so they use describe more properly to group them without test's name redundancy

--- server/dao/journal_dao.test.js
+++ server/dao/journal_dao.test.js
@@ -68,57 +68,59 @@ beforeAll(() => {
         .run(user.username, String(existing_empty_day.date));
 });
 
-describe("Journal.get_day", () => {
-    test("JournalDAO.get_day: success", () => {
-        expect(journal_dao.get_day(user, existing_day.date)).toEqual(existing_day);
-    });
-    
-    test("JournalDAO.get_day: no data found (null)", () => {
-        expect(journal_dao.get_day(user, new_day.date)).toBe(null);
-    });
-    
-    test("JournalDAO.get_day: InvalidUser", () => {
-        expect(() => journal_dao.get_day(invalid_user, existing_day.date)).toThrow(InvalidUser);
-    });
-
-    describe("Jouranl.get_day private functions", () => {
-        test("JournalDAO._check_if_user_exists: success", () => {
-            expect(journal_dao._check_if_user_exists(user)).toBeTruthy();
-            expect(journal_dao._check_if_user_exists(invalid_user)).toBeFalsy();
+describe("JournalDAO", () => {
+    describe("get_day", () => {
+        test("Success", () => {
+            expect(journal_dao.get_day(user, existing_day.date)).toEqual(existing_day);
         });
         
-        test("JournalDAO._get_mood_fields_id: success", () => {
-            expect(journal_dao._get_mood_fields_id(user, existing_day.date)).toBeTruthy();
-            expect(journal_dao._get_mood_fields_id(user, new_day.date)).toBeFalsy();
+        test("No data found (null)", () => {
+            expect(journal_dao.get_day(user, new_day.date)).toBe(null);
         });
         
-        test("JournalDAO._get_mood_fields: success", () => {
-            const mood_fields_id = journal_dao._get_mood_fields_id(user, existing_day.date);
-            const result = journal_dao._get_mood_fields(mood_fields_id)
-                                        .sort((a, b) => a.get_field_name().localeCompare(b.get_field_name()));
-            const expected = existing_day.mood_fields
-                                        .sort((a, b) => a.get_field_name().localeCompare(b.get_field_name()));
-            expect(result).toEqual(expected);
+        test("InvalidUser", () => {
+            expect(() => journal_dao.get_day(invalid_user, existing_day.date)).toThrow(InvalidUser);
         });
-        
-        test("JournalDAO._get_activities: success", () => {
-            //Uses schedule to make sure they're sorted properly
-            const result_schedule = new Schedule(journal_dao._get_activities(user, existing_day.date));
-            expect(result_schedule.get_activities()).toEqual(existing_day.schedule.get_activities());
+
+        describe("Private functions", () => {
+            test("_check_if_user_exists: success", () => {
+                expect(journal_dao._check_if_user_exists(user)).toBeTruthy();
+                expect(journal_dao._check_if_user_exists(invalid_user)).toBeFalsy();
+            });
+            
+            test("_get_mood_fields_id: success", () => {
+                expect(journal_dao._get_mood_fields_id(user, existing_day.date)).toBeTruthy();
+                expect(journal_dao._get_mood_fields_id(user, new_day.date)).toBeFalsy();
+            });
+            
+            test("_get_mood_fields: success", () => {
+                const mood_fields_id = journal_dao._get_mood_fields_id(user, existing_day.date);
+                const result = journal_dao._get_mood_fields(mood_fields_id)
+                                            .sort((a, b) => a.get_field_name().localeCompare(b.get_field_name()));
+                const expected = existing_day.mood_fields
+                                            .sort((a, b) => a.get_field_name().localeCompare(b.get_field_name()));
+                expect(result).toEqual(expected);
+            });
+            
+            test("_get_activities: success", () => {
+                //Uses schedule to make sure they're sorted properly
+                const result_schedule = new Schedule(journal_dao._get_activities(user, existing_day.date));
+                expect(result_schedule.get_activities()).toEqual(existing_day.schedule.get_activities());
+            });
+        })
+
+    });
+
+    describe("create_day", () => {
+        test("success", () => {
+            journal_dao.create_day(user, new_day);
+            expect(journal_dao.get_day(user, new_day.date)).toEqual(new_day);
         });
-    })
-
-});
-
-describe("Journal.create_day", () => {
-    test("create_day: success", () => {
-        journal_dao.create_day(user, new_day);
-        expect(journal_dao.get_day(user, new_day.date)).toEqual(new_day);
+        test("InvalidUser", () => {
+            expect(() => journal_dao.create_day(invalid_user, new_day)).toThrow(InvalidUser);
+        });
+        test("DayAlreadyExists", () => {
+            expect(() => journal_dao.create_day(user, existing_day)).toThrow(DayAlreadyExists);
+        });
     });
-    test("create_day: InvalidUser", () => {
-        expect(() => journal_dao.create_day(invalid_user, new_day)).toThrow(InvalidUser);
-    });
-    test("create_day: DayAlreadyExists", () => {
-        expect(() => journal_dao.create_day(user, existing_day)).toThrow(DayAlreadyExists);
-    });
-});
+})

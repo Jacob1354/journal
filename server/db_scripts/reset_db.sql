56f8ee3ba97cf765244ff40535cbd6f23d6af981
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-09-04 19:48:23 -0400

    Fix(server/reset_db.sql): Change the order of the deletion of data to make sure there isn't any issues related to foreign keys

--- server/db_scripts/reset_db.sql
+++ server/db_scripts/reset_db.sql
@@ -1,8 +1,8 @@
-DELETE FROM user;
-DELETE FROM session;
 DELETE FROM activity;
 DELETE FROM number_field;
 DELETE FROM fraction_field;
 DELETE FROM text_field;
 DELETE FROM slider_field;
 DELETE FROM mood_fields;
+DELETE FROM session;
+DELETE FROM user;

a0591a321f1ca493f705e04f282e1d92fa03d677
Salois, Jacob <salois.jacob@courrier.uqam.ca>
2025-09-05 21:14:10 -0400

    Feat(public/day): Add the signout eventHandler on click of the sign out button. No route to handle it server side, yet

--- public/pages/day/js/day.js
+++ public/pages/day/js/day.js
@@ -1,6 +1,8 @@
 import { DomDay } from "./dom/dom_day.js"
 import { get_day } from "./api/day_api.js";
+import { init } from "./utils.js";
+
+init();
 
 let dom_day = new DomDay(await get_day());
-
 dom_day.render_day();
